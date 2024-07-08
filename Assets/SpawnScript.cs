4be900dab15d15e6ba4afbf0e9430931fc10c506
denislagut <125026324+denislagut@users.noreply.github.com>
2024-07-08 21:43:19 +0300

    Волны

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BotSpawner : MonoBehaviour
 {
@@ -8,29 +10,77 @@ public class BotSpawner : MonoBehaviour
     public GameObject botPrefab2;
     public Transform[] spawnPoints;
 
-    public float spawnInterval = 5f;
-    private float timer = 0f;
+    public TextMeshProUGUI WaveText;
+
+    public int numberOfEnemies = 2;
+    public int nowTheEnemies = 0;
+
+    private static int waveNumber = 0;
+    private int indexBot;
+    private float timer = 0;
 
     private void Update()
     {
-        TimerSpawn();
+        
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            if (SceneManager.GetActiveScene().name == "GameScene") TimerSpawn();
+            timer = 0;
+        }
     }
 
     private void TimerSpawn()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= spawnInterval)
+        nowTheEnemies = GameObject.FindGameObjectsWithTag("Bot").Length;
+        if (nowTheEnemies == 0)
         {
-            SpawnBot();
-            timer = 0f;
+            numberOfEnemies += 2;
+            nowTheEnemies = numberOfEnemies;
+            waveNumber++;
+            WaveText.text = "" + waveNumber;
+            if (waveNumber <= 2)
+            {
+                for (int i = 0; i < numberOfEnemies; i++)
+                {
+                    indexBot = 0;
+                    SpawnBot(indexBot);
+                }
+            }
+            else
+            {
+                if (waveNumber == 3)
+                {
+                    for (int i = 0; i < numberOfEnemies; i++)
+                    {
+                        indexBot = Random.Range(0, 3);
+                        if (indexBot < 3) indexBot = 0;
+                        if (indexBot == 3) indexBot = 1;
+                        SpawnBot(indexBot);
+                    }
+                }
+                else
+                {
+                    if (waveNumber <= 5)
+                    {
+                        for (int i = 0; i < numberOfEnemies; i++)
+                        {
+                            indexBot = Random.Range(0, 3);
+                            if (indexBot < 2) indexBot = 0;
+                            if (indexBot == 2) indexBot = 1;
+                            SpawnBot(indexBot);
+                        }
+                    }
+                }
+            }
         }
     }
 
-    private void SpawnBot()
+    private void SpawnBot(int indexBot)
     {
         Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        switch (Random.Range(0, 2)) {
+        switch (indexBot)
+        {
             case 0:
                 {
                     Instantiate(botPrefab1, randomSpawnPoint.position, randomSpawnPoint.rotation);
